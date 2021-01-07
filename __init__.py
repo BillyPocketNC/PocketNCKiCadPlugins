59077b66574ddaff49c1b38969e6ce5b0d4618c3
Billy Bogardus <billy@pocketnc.com>
2021-01-07 12:20:14 -0700

    updated init

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from kicad_scripts import *;
-from kicad_action_Plugins import *;
 from kiactionsmitjaNemec import *;
+from easyw_ki_Action import *;
+from NilujePerchut_ki_action import *;
 from RF_tools_KiCAD import *;
